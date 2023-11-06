e4003c25d9fa965b2586d590e4f64239a1c0dc18
rezniki <igorreznik2001@gmail.com>
2023-11-06 21:51:24 +0300

    Clothes / add modal window for Checkout

--- src/components/common/Clothes/Clothes.jsx
+++ src/components/common/Clothes/Clothes.jsx
@@ -5,14 +5,24 @@ import Busketfull from '../../../img/black-basket.svg';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import i18n from '../../common/LanguageSelector/i18n.js';
+import ReactCreditCards from 'react-credit-cards-2';
+import 'react-credit-cards-2/dist/es/styles-compiled.css';
 
 const Clothes = (props) => {
     const [isModalShow, setModalShow] = useState(false);
     const [currentCard, setCurrentCard] = useState();
     const [isCartModalShow, setCartModalShow] = useState(false);
     const [selectedCard, setSelectedCard] = useState(null);
+    const [isCheckOut, setCheckOut] = useState(false);
     const { t } = useTranslation('translation', { i18n });
     const [showChangeLanguage, setShowChangeLanguage] = useState();
+    const [state, setState] = useState({
+        number: '',
+        expiry: '',
+        cvc: '',
+        name: '',
+        focus: '',
+    });
 
     const changeLanguage = (lng) => {
         i18n.changeLanguage(lng);
@@ -39,6 +49,16 @@ const Clothes = (props) => {
         setModalShow(false);
         setCartModalShow(true);
     } 
+
+    const handleInputChange = (evt) => {
+        const { name, value } = evt.target;
+        setState((prev) => ({ ...prev, [name]: value }));
+    };
+
+    const handleInputFocus = (evt) => {
+        setState((prev) => ({ ...prev, focus: evt.target.name }));
+    };
+
 
     return (
         <>
@@ -76,7 +96,74 @@ const Clothes = (props) => {
                 </div>
                 <div className='cart__summary'>
                     <p className='cart__summary__cost'>Summary: {selectedCard.cost}$</p>
-                    <img className='cart__payment' src={Busketfull} alt='Busketfull'/>
+                    <img className='cart__payment' src={Busketfull} alt='Busketfull' onClick={() => {
+                        setCartModalShow(false);
+                        setCheckOut(true);
+                    }}/>
+                </div>
+            </Modal>}
+            {isCheckOut && <Modal title='Checkout' modalCloseClick={() => setCheckOut(false)}>
+                <div className='checkout__credit__card'>
+                    <ReactCreditCards 
+                        number={state.number} 
+                        expiry={state.expiry}
+                        cvc={state.cvc}
+                        name={state.name}
+                        focused={state.focus}
+                    />
+                </div>
+                <div className='checkout__form__groups'>
+                    <form className='checkout__form__group'>
+                        <input
+                            className='checkout__form__control'
+                            type="number"
+                            name="number"
+                            placeholder="Card Number"
+                            pattern="[\d|]{16,22}"
+                            required
+                            value={state.number}
+                            onChange={handleInputChange}
+                            onFocus={handleInputFocus}
+                        />
+                    </form>
+
+                    <form className='checkout__form__group'>
+                        <input
+                            className='checkout__form__control'
+                            type="text"
+                            name="name"
+                            placeholder="Name"
+                            required
+                            onChange={handleInputChange}
+                            onFocus={handleInputFocus}
+                        />
+                    </form>
+
+                    <div className='checkout__form__row'>
+                        <form className='checkout__form__data'>
+                            <input 
+                                type="tel"
+                                name="expiry"
+                                placeholder="Valid Thru"
+                                pattern="\d\d/\d\d"
+                                required
+                                onChange={handleInputChange}
+                                onFocus={handleInputFocus}
+                            />
+                        </form>
+
+                        <form className='checkout__form__data'>
+                            <input 
+                                type="tel"
+                                name="cvc"
+                                placeholder="CVC"
+                                pattern="\d{3,4}"
+                                required
+                                onChange={handleInputChange}
+                                onFocus={handleInputFocus}
+                            />
+                        </form>
+                    </div>
                 </div>
             </Modal>}
         </>
