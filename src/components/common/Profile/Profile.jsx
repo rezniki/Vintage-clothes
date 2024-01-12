6c8726806df99c6b063cd84205514f43892d75a3
rezniki <igorreznik2001@gmail.com>
2024-01-12 16:45:06 +0300

    Profile, ProfilePage / add in page ProfilePage block fullname and job

--- src/components/common/Profile/Profile.jsx
+++ src/components/common/Profile/Profile.jsx
@@ -9,7 +9,7 @@ const Profile = (props) => {
                 props.cardsUser.map((cardUser) => {
                     return (
                         <div key={cardUser.id} className='card__profile'>
-                            <Link className='card__profile__url' to={`/profile/${cardUser.id}`}>
+                            <Link className='card__profile__url' to={`/profile/${cardUser.id}`} state={{avatar: cardUser.avatar, fullname: cardUser.name, job: cardUser.job, salary: cardUser.salary}}>
                                 <img className='card__profile__image' src={cardUser.avatar} alt="Avatar" />
                                 <p className='card__profile__fullname'>{cardUser.name}</p>
                             </Link>
--- src/components/pages/ProfilePage/ProfilePage.jsx
+++ src/components/pages/ProfilePage/ProfilePage.jsx
@@ -1,9 +1,43 @@
 import './style.css';
+import { useParams, useLocation } from 'react-router-dom';
 
 const ProfilePage = () => {
+
+    const { id } = useParams();
+    const location = useLocation();
+
+    const avatar = location.state.avatar;
+    const fullname = location.state.fullname;
+    const job = location.state.job;
+    const salary = location.state.salary;
+
+    console.log(id);
+
     return (
         <>
-            <p className='header__profile'>Hello world</p>
+            <div className='profile__page'>
+                <div className='profile__page__people'>
+                    <img className='profile__page__image' src={avatar} alt="Avatar" />
+                    <div className='profile__fullname__job'>
+                        <h1 className='profile__page__fullname'>{fullname}</h1>
+                        <p className='profile__page__job'>{job}</p>
+                    </div>
+                    <button className='profile__page__edit'>Edit team</button>
+                </div>
+                <div className='profile__details__info'>
+                    <div className='profile__details__tasks'>
+                        <h3 className='profile__task__title'>Tasks</h3>
+                        <p className='profile__details__language'>english</p>
+                        <p className='profile__english__update'>update english to B2</p>
+                        <p className='profile__position__title'>position</p>
+                        <p className='profile__details__position'>became S1+</p>
+                        <p className='profile__details__salary'>${salary}</p>
+                    </div>
+                    <div className='profile__details__infomation'>
+                        <p className='profile__information__title'>Information</p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
