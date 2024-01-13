2357608e86b34c7f7fa584deb31bab9d73244e62
rezniki <igorreznik2001@gmail.com>
2024-01-13 19:16:39 +0300

    Profile, ProfilePage / add block fullname and job user

--- src/components/pages/ProfilePage/ProfilePage.jsx
+++ src/components/pages/ProfilePage/ProfilePage.jsx
@@ -1,26 +1,33 @@
 import './style.css';
-import { useParams, useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const ProfilePage = () => {
 
     const { id } = useParams();
-    const location = useLocation();
-
-    const avatar = location.state.avatar;
-    const fullname = location.state.fullname;
-    const job = location.state.job;
-    const salary = location.state.salary;
+    const [user, setUser] = useState(null);
 
     console.log(id);
+    
+    useEffect(() => {
+        fetch(`https://657b3715394ca9e4af13fd34.mockapi.io/profiles/${id}`)
+            .then(response => response.json())
+            .then(data => setUser(data))
+            .catch(error => console.error('Load error profile user', error));
+    }, [id]);
+
+    if (!user) {
+        return <p>Loading...</p>
+    }
 
     return (
         <>
             <div className='profile__page'>
                 <div className='profile__page__people'>
-                    <img className='profile__page__image' src={avatar} alt="Avatar" />
+                    <img className='profile__page__image' src={user.avatar} alt="Avatar" />
                     <div className='profile__fullname__job'>
-                        <h1 className='profile__page__fullname'>{fullname}</h1>
-                        <p className='profile__page__job'>{job}</p>
+                        <h1 className='profile__page__fullname'>{user.name}</h1>
+                        <p className='profile__page__job'>{user.job}</p>
                     </div>
                     <button className='profile__page__edit'>Edit team</button>
                 </div>
@@ -31,7 +38,7 @@ const ProfilePage = () => {
                         <p className='profile__english__update'>update english to B2</p>
                         <p className='profile__position__title'>position</p>
                         <p className='profile__details__position'>became S1+</p>
-                        <p className='profile__details__salary'>${salary}</p>
+                        <p className='profile__details__salary'>${user.salary}</p>
                     </div>
                     <div className='profile__details__infomation'>
                         <p className='profile__information__title'>Information</p>
